6f37a2bd074cfe5f28672ca50e1e8649ec33fa7e
Joao Abreu <jp.dellabianca@.gmail.com>
2022-02-23 07:02:37 -0300

    eslint update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Header from './components/Header';
 
 const App: React.FC = () => {
-  return <div>e</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default App;
