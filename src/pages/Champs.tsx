8f054774192f9ce9e9f6cfb880709ef6b27ab92d
Joao Abreu <jp.dellabianca@.gmail.com>
2022-02-24 03:32:53 -0300

    Add pages

---
+++ src/pages/Champs.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Champs: React.FC = () => {
+  return <div>Champs</div>;
+};
+
+export default Champs;
---
+++ src/pages/FavoritesChamps.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FavoritesChamps: React.FC = () => {
+  return <div>FavoritesChamps</div>;
+};
+
+export default FavoritesChamps;
