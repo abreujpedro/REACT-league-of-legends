71bba344a5ca9805f5c2aaf080f36f67d252eb1e
Joao Abreu <jp.dellabianca@.gmail.com>
2022-02-24 05:16:04 -0300

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Header from "./components/header/Header";
+import Champs from "./pages/Champs";
+import FavoritesChamps from "./pages/FavoritesChamps";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Register from "./pages/Register";
 
 const App: React.FC = () => {
   return (
-    <div>
+    <BrowserRouter>
       <Header />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/champs" element={<Champs />} />
+        <Route path="favoritesChamps" element={<FavoritesChamps />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
