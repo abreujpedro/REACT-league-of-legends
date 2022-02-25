7e905189a9fa18ade7456f055095016c77f5f731
Joao Abreu <jp.dellabianca@.gmail.com>
2022-02-25 06:20:14 -0300

    Add move picture with mouse

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,14 +5,46 @@ import Container from "../components/container/Container";
 import styles from "./Home.module.css";
 
 const Home: React.FC = () => {
+  const pictureMainRef = React.useRef<HTMLElement | null>(null);
+  const pictureDivRef = React.useRef<HTMLDivElement | null>(null);
+
+  React.useEffect(() => {
+    const setPictureSize = () => {
+      pictureMainRef.current
+        ? (pictureMainRef.current.style.width = `${window.innerWidth + 300}px`)
+        : null;
+    };
+
+    setPictureSize();
+    window.addEventListener("resize", setPictureSize);
+    return () => {
+      window.removeEventListener("resize", setPictureSize);
+    };
+  }, []);
+
+  React.useEffect(() => {
+    const setScroll = (e: MouseEvent) => {
+      pictureDivRef.current
+        ? (pictureDivRef.current.scrollLeft = e.pageX / 10)
+        : null;
+    };
+
+    window.addEventListener("mousemove", setScroll);
+    return () => {
+      window.removeEventListener("mousemove", setScroll);
+    };
+  }, []);
+
   return (
     <>
       <section>
-        <main className={styles.main}>
-          <Container>
-            <h1>Favorites Champs</h1>
-          </Container>
-        </main>
+        <div ref={pictureDivRef} className={styles.pictureContainer}>
+          <main className={styles.main} ref={pictureMainRef}>
+            <Container>
+              <h1>Favorites Champs</h1>
+            </Container>
+          </main>
+        </div>
       </section>
       <section>
         <Container>
